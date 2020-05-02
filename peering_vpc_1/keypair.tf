c7948e8d3c94c6a9e06b8900c05865bab97b098c
azymberdi <azymberdi@gmail.com>
2020-05-02 06:17:06 +0000

    personal changes

--- peering_vpc_1/keypair.tf
+++ peering_vpc_1/keypair.tf
@@ -1,5 +1,6 @@
 resource "aws_key_pair" "local" {
   key_name   = "windows"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3FQ2PpzuZb4AxCzNBUq5GCDg94TVMt0zXZ+1dOcNwJs78RN75lRUCQPM5xCXBRx1DCdP33bsN9N0sE5KZxKQt4v+VqLGViuoGubMqebimxboeoOan8BVtW29o3QmI/8IBvQodNK5q5VFYS/RDrp7lRDAwBaA+By5unezx7yC9aSgA6QNEDysmrcNX0Af/MN7DmjlF2D97+GSXlqQQ28GTNCpqw+gvdb4SHCTD4wezxzHafdcjX9pPPi7AOU0vwHZD3JK00Z/9DyW9MmqWSYIeEYLEfHCjmloXNbPKCh7ufwg/lY3G+tGVBrs6bWqcrJ/18TEQ+1gZsT/Z2Va6TwSH adnank@cyberkursun"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
+
 
--- peering_vpc_1/user_data1.sh
+++ peering_vpc_1/user_data1.sh
@@ -2,4 +2,4 @@
 sudo yum install httpd -y
 sudo systemctl enable httpd 
 sudo systemctl start httpd
-echo "<html><h1>vpc_vpc_peering Task by Adnan</h1></html>" | sudo tee /var/www/html/index.html
+echo "<Task 2 Completed>" | sudo tee /var/www/html/index.html
